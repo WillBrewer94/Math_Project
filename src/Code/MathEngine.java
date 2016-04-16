b4edc77840705238bd5cc131a7a6406487e562b7
Will Brewer <willbrewer182@gmail.com>
2016-04-15 22:51:15 -0400

    Initial Commit
    ayy lmao

---
+++ src/Code/MathEngine.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Will on 4/15/2016.
+ */
+package Code;
+
+public class MathEngine {
+    public static void main(String[] args) {
+        MathEngine m = new MathEngine();
+        m.run();
+    }
+
+    public void run() {
+
+    }
+}
